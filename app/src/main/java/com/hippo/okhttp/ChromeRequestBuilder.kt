74cd382067b6febe32494d0ae2de746fe6509346
adnotfound <adnotfound@gmail.com>
2023-10-28 15:34:56 +0800

    Setting up the latest User Agent

--- app/src/main/java/com/hippo/okhttp/ChromeRequestBuilder.kt
+++ app/src/main/java/com/hippo/okhttp/ChromeRequestBuilder.kt
@@ -20,7 +20,7 @@ import okhttp3.Request
 open class ChromeRequestBuilder(url: String) : Request.Builder() {
     companion object {
         private const val CHROME_USER_AGENT =
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54"
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.35 Safari/537.36 Edg/117.0.5938.35"
         private const val CHROME_ACCEPT =
             "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
         private const val CHROME_ACCEPT_LANGUAGE = "en-US,en;q=0.9"
