d8ff5aea55fe1fe24d2e6ec55648814018f5df7f
AdNotFound <adnotfound@gmail.com>
2023-10-28 21:26:02 +0800

    Bump android and com.google.devtools.ksp from 1.8.21 to 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.1.0-beta04" apply false
-    kotlin("android") version "1.8.21" apply false
-    id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
+    kotlin("android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
 }
 
 allprojects {
