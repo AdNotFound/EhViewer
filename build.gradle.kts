258a7f1119483e5d85de8a72c47b824e3b936dc0
AdNotFound <adnotfound@gmail.com>
2023-11-04 12:20:51 +0800

    bump com.google.devtools.ksp to 1.9.20-1.0.14
    chore(deps): bump com.google.devtools.ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.1.2" apply false
     kotlin("android") version "1.9.20" apply false
-    id("com.google.devtools.ksp") version "1.9.20-1.0.13" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
     id("com.diffplug.spotless") version "6.22.0" apply false
 }
 
