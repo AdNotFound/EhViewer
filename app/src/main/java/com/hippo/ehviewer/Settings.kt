440ff5ae46eb0d764d0136f5c4dabc36a5059038
AdNotFound <adnotfound@gmail.com>
2024-01-02 16:51:56 +0800

    !For test ReAdd DoH

--- app/src/main/java/com/hippo/ehviewer/Settings.kt
+++ app/src/main/java/com/hippo/ehviewer/Settings.kt
@@ -178,6 +178,8 @@ object Settings {
     private const val DEFAULT_PROXY_PORT = -1
     private const val KEY_BUILT_IN_HOSTS = "built_in_hosts_2"
     private const val DEFAULT_BUILT_IN_HOSTS = false
+    private const val KEY_DOH = "dns_over_https"
+    private const val DEFAULT_DOH = false
     const val KEY_DOMAIN_FRONTING = "domain_fronting"
     private const val DEFAULT_DOMAIN_FRONTING = false
     const val KEY_BYPASS_VPN = "bypass_vpn"
@@ -662,6 +664,12 @@ object Settings {
         get() = getBoolean(KEY_BUILT_IN_HOSTS, DEFAULT_BUILT_IN_HOSTS)
     fun putBuiltInHosts(value: Boolean) {
         putBoolean(KEY_BUILT_IN_HOSTS, value)
+    }
+
+    val dOH: Boolean
+        get() = getBoolean(KEY_DOH, DEFAULT_DOH)
+    fun putDoH(value: Boolean) {
+        putBoolean(KEY_DOH, value)
     }
 
     val dF: Boolean
