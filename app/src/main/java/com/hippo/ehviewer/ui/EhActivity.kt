d5d02c397de379ee571c4e3ccc04ec84814d494a
AdNotFound <148407324+AdNotFound@users.noreply.github.com>
2026-01-17 15:42:34 +0800

    Revert "feat: Implement accent color picker"
    This reverts commit 15f0673384780cdef2d886dd64cf0dbc47864154.

--- app/src/main/java/com/hippo/ehviewer/ui/EhActivity.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/EhActivity.kt
@@ -43,7 +43,6 @@ abstract class EhActivity : AppCompatActivity() {
     override fun onApplyThemeResource(theme: Theme, resid: Int, first: Boolean) {
         theme.applyStyle(resid, true)
         theme.applyStyle(getThemeStyleRes(), true)
-        theme.applyStyle(ThemeColors.fromKey(Settings.themeColor).styleRes, true)
     }
 
     override fun onNightModeChanged(mode: Int) {
