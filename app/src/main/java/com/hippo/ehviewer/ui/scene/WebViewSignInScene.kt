b6067bdcab5cec97b0c5a0d1296a706599e056b5
AdNotFound <148407324+AdNotFound@users.noreply.github.com>
2026-06-20 15:21:57 +0800

    fix(ux): handle back navigation in WebView sign-in

--- app/src/main/java/com/hippo/ehviewer/ui/scene/WebViewSignInScene.kt
+++ app/src/main/java/com/hippo/ehviewer/ui/scene/WebViewSignInScene.kt
@@ -45,6 +45,14 @@ class WebViewSignInScene : SolidScene() {
 
     override fun needShowLeftDrawer(): Boolean = false
 
+    override fun onBackPressed() {
+        if (mWebView?.canGoBack() == true) {
+            mWebView?.goBack()
+        } else {
+            finish()
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
